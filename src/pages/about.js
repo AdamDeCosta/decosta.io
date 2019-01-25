8ee346ee3c9869e773622558359f173a42136f29
Adam DeCosta <adam@decosta.io>
2019-01-25 18:19:38 -0500

    Added some about text and added an avatar, needs more styling.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,22 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Image from '../components/image'
 
 import '../components/about.scss'
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <section className="about" />
+    <section className="about">
+      <div className="about__container">
+        <div className="about__image">
+          <Image />
+        </div>
+        Hey I'm Adam DeCosta. I'm a third year Computer Science & Innovation
+        major studying at Champlain College. 
+      </div>
+    </section>
   </Layout>
 )
 
